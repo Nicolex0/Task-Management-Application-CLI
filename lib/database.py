f3fcec8a9b0cd4c1aa8a9d6af6e05e10953bb83e
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-06-12 21:47:44 +0300

    Add task manager.db for task management database storage

--- lib/database.py
+++ lib/database.py
@@ -1,5 +1,8 @@
-# dd necessary SQLAlchemy and OS imports for database setup
+# Add necessary SQLAlchemy and OS imports for database setup
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+
+# Add task_manager.db for task management database storage
+DATABASE_FILE = "task_manager.db"
