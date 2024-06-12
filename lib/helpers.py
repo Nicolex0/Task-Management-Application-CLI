e23b25cbeda2eb91b3bc912a4ffaa8be9d98552d
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-12 17:30:44 +0300

    Create function to get user by user name and to delete user

--- lib/helpers.py
+++ lib/helpers.py
@@ -10,3 +10,14 @@ def create_user(username, email, password):
 # Create function to get user by id
 def get_user_by_id(user_id):
     return session.query(User).filter(User.id == user_id).first()
+
+def get_user_by_username(username):
+    return session.query(User).filter(User.username == username).first()
+
+def delete_user(user_id):
+    user = session.query(User).filter(User.id == user_id).first()
+    if user:
+        session.delete(user)
+        session.commit()
+        return True
+    return False
