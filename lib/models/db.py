edf9679c337994ebf0799c7834fd544598b15362
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-06-12 16:51:14 +0300

    Set up the engine class and the base class

--- lib/models/db.py
+++ lib/models/db.py
@@ -5,3 +5,9 @@ from sqlalchemy.orm import sessionmaker, relationship
 # Set up the database URL
 DATABASE_URL = 'sqlite:///task_manager.db'
 
+# Set up the engine class and the base class
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+Base = declarative_base()
+
+
+
