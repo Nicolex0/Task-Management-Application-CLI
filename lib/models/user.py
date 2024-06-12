a38e6411d43b3da7f927bc9afef5933b649b0b0b
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-06-12 16:41:38 +0300

    Create the User class

---
+++ lib/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from .db import Base  
+
+# Create user class
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    tasks = relationship("Task", back_populates="assignee") 
