ac2141fd6102005294010b11c5f47e57a5ef4296
Nicole Apono <napononicole@gmail.com>
2024-06-12 14:50:02 +0300

    Create function for user management menu

--- lib/cli.py
+++ lib/cli.py
@@ -25,3 +25,25 @@ def menu():
     print("1. User Management")
     print("2. Task Management")
     print("3. Task Assignment and Filtering")
+
+def user_management_menu():
+    while True:
+        print("\nUser Management Menu:")
+        print("1. Create User")
+        print("2. Delete User")
+        print("3. View All Users")
+        print("4. Back to Main Menu")
+        choice = input("> ")
+        if choice == "1":
+            create_user_menu()
+        elif choice == "2":
+            delete_user_menu()
+        elif choice == "3":
+            view_all_users()
+        elif choice == "4":
+            return
+        else:
+            print("Invalid choice")
+
+def task_management_menu():
+    pass
