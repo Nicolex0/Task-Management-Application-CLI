6c4790e89b026c0b9b6e8b8f80a03dbca63886d9
Nicole Apono <napononicole@gmail.com>
2024-06-12 14:45:38 +0300

    define menu

--- lib/cli.py
+++ lib/cli.py
@@ -3,3 +3,18 @@
 import sys
 from lib.helpers import *
 from lib.models import session
+
+def main():
+    while True:
+        menu()
+        choice = input("> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            user_management_menu()
+        elif choice == "2":
+            task_management_menu()
+        elif choice == "3":
+            task_assignment_and_filtering_menu()
+        else:
+            print("Invalid choice")
